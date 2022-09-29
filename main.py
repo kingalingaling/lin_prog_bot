5646905beaf666b5134afda9a222fb4ace053afa
King <41597003+kingalingaling@users.noreply.github.com>
2022-09-29 14:52:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import telebot
 load_dotenv()
 
 API_KEY = os.environ.get('API_KEY')
-#API_KEY = '5790584247:AAEKgvTOGM_BWZ5m8FBNgGkaFnp6iXDjVXI'
 bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['greet'])
