d1da61367865f4db4e1ddf2ad7f0c00d5af7c0bd
King Eshiebor <kingeshiebor@gmail.com>
2022-09-30 15:50:22 +0100

    Somewhat straightforward approach

---
+++ lin_prog.py
@@ -0,0 +1,26 @@
+import pulp as p
+  
+def lin_prog(cost_a, cost_b, profit_a, profit_b, budget, item_a='Item A', item_b='Item B', soln='Maximize'):
+    # Create a LP Minimization problem
+    if soln == 'Maximize':
+        Lp_prob = p.LpProblem('Problem', p.LpMaximize)
+    else:
+        Lp_prob = p.LpProblem('Problem', p.LpMinimize)
+        
+    # Create problem Variables 
+    x = p.LpVariable(item_a, lowBound = 0)   # Create a variable x >= 0
+    y = p.LpVariable(item_b, lowBound = 0)   # Create a variable y >= 0
+    # Objective Function
+    Lp_prob += profit_a * x + profit_b * y   
+    # Constraints:
+    Lp_prob += cost_a * x + cost_b * y <= 500
+    Lp_prob += x + y <= int(budget/max((cost_a, cost_b)))
+  
+    # Display the problem
+    #print(Lp_prob)
+  
+    status = Lp_prob.solve()   # Solver
+    #print(p.LpStatus[status])   # The solution status
+    
+    # Printing the final solution
+    return p.value(x), p.value(y), p.value(Lp_prob.objective)
--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-import os
+import os, sys
+from pkgutil import get_data
 from dotenv import load_dotenv
-import telebot
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
 load_dotenv()
 
+#setup token
 API_KEY = os.environ.get('API_KEY')
-#API_KEY = '5790584247:AAEKgvTOGM_BWZ5m8FBNgGkaFnp6iXDjVXI'
-bot = telebot.TeleBot(API_KEY)
+updater = Updater(API_KEY, use_context=True)
 
-@bot.message_handler(commands=['greet'])
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}, and welcome to lin_prog_bot; a bot that helps you make the right decisions using linear and integer programming')
 
-def greet(message):
-    bot.reply_to(message, 'Why hello unto you too!!')
+#make hello command run the helo function
+updater.dispatcher.add_handler(CommandHandler('start', start))
 
-bot.infinity_polling()
+#HELP COMMAND
+def help(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'''
+    Here's a list of all the commands you can issue to the bot and thir descriptions.
+    ''')
+
+updater.dispatcher.add_handler(CommandHandler('help', help))
+
+def lin_programming(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'You have decided to solve a Linear programming problem. What is the cost of A?')
+
+updater.dispatcher.add_handler(CommandHandler('lin_programming', lin_programming))
+
+def calc(update: Update, context: CallbackContext) -> None:
+    vals = update.message.text.split(',')
+    if len(vals) == 5:
+        try:
+            cost_a = float(vals[0])
+            profit_a = float(vals[1])
+            cost_b = float(vals[2])
+            profit_b = float(vals[3])
+            budget = float(vals[4])
+        except:
+            update.message.reply_text('Pleaase type in numbers only. Try dey pay attention!!!')
+            sys.exit(1)
+        from lin_prog import lin_prog
+        x, y, z = lin_prog(cost_a, cost_b, profit_a, profit_b, budget, soln="Maximize")
+        update.message.reply_text(f'Number of item  A to buy: {x}\n number of Item B to buy: {y} \n Total profit: {z}')
+    else:
+        update.message.reply_text('You should type in exactly five numbers separates by commas and no space')
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, calc))
+
+#connect to Telegram and wait for messages
+updater.start_polling()
+
+#keep programming running until interrupted
+updater.idle()
